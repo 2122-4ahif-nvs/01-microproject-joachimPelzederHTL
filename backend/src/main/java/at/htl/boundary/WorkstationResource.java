508cd219f45750f8542dea85800c0992fbf673f9
joachimPelzederHTL <jo.pelzeder@gmail.com>
2022-02-10 18:18:44 +0100

    #2-workstation-with-parameter-declaration-inside-template

--- backend/src/main/java/at/htl/boundary/WorkstationResource.java
+++ backend/src/main/java/at/htl/boundary/WorkstationResource.java
@@ -2,10 +2,13 @@ package at.htl.boundary;
 
 import at.htl.control.WorkstationRepository;
 import at.htl.entity.Workstation;
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -16,9 +19,19 @@ public class WorkstationResource {
     @Inject
     WorkstationRepository workstationRepository;
 
+    @Inject
+    Template workstation;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Workstation> getAll(){
         return workstationRepository.findAll().list();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get(@PathParam("id") Long id){
+        return workstation.data("workstation", workstationRepository.findById(id));
     }
 }
