3bfde8d616fc38295d3017f11a001eb9d3bc93a3
joachimPelzederHTL <jo.pelzeder@gmail.com>
2022-02-10 17:46:11 +0100

    #1-JSON-mapping and validation

--- backend/src/main/java/at/htl/boundary/EmployeeResource.java
+++ backend/src/main/java/at/htl/boundary/EmployeeResource.java
@@ -4,11 +4,17 @@ import at.htl.control.EmployeeRepository;
 import at.htl.entity.Employee;
 
 import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.transform.Result;
+import javax.xml.validation.Validator;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Path("employee")
 public class EmployeeResource {
@@ -16,9 +22,49 @@ public class EmployeeResource {
     @Inject
     EmployeeRepository employeeRepository;
 
+    @Inject
+    Validator validator;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public List<Employee> getAll(){
         return employeeRepository.findAll().list();
+    }
+
+    @Path("/manual-validation")
+    @POST
+    public Result tryMeManualValidation(Employee employee){
+        Set<ConstraintViolation<Employee>> violations = validator.validate(employee);
+        if(violations.isEmpty()){
+            return new Result("Employee is valid! It was validated by manual validation.");
+        } else {
+            return new Result(violations);
+        }
+    }
+
+    public static class Result{
+
+        private boolean success;
+        private String message;
+
+        public Result(String message) {
+            this.message = message;
+            this.success = true;
+        }
+
+        Result(Set<? extends ConstraintViolation<?>> violations){
+            this.success = false;
+            this.message = violations.stream()
+                    .map(cv -> cv.getMessage())
+                    .collect(Collectors.joining(", "));
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public String getMessage() {
+            return message;
+        }
     }
 }
