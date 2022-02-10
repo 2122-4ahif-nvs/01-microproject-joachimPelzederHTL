529b8d35891024d2cec379e3837b58ab486b03a1
joachimPelzederHTL <jo.pelzeder@gmail.com>
2022-02-10 19:08:10 +0100

    #3-HelloRepoTest

---
+++ backend/src/test/at/htl/control/HelloRepositoryTest.java
@@ -0,0 +1,23 @@
+package at.htl.control;
+
+import io.quarkus.grpc.GrpcClient;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloRepositoryTest implements Greeter{
+
+    @GrpcClient
+    Greeter client;
+
+    @Test
+    void shouldReturnHello() {
+        CompletableFuture<String> message = new CompletableFuture<>();
+        client.sayHello(HelloRequest.newBuilder().setName("Quarkus").build())
+                .subscribe().with(reply -> message.complete(reply.getMessage()));
+        assertThat(message.get(5, TimeUnit.SECONDS)).isEqualTo("Hello Quarkus");
+    }
+}
